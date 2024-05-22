2a41aeed8f0c7ddf238c80317ab1895c6dc59b43
Ruchi Zawar <ruchi.zawar21@gmail.com>
2024-05-22 00:53:34 +0000

    Comparison and equality check

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score = "33" // "33abc" / null / undefined
+/*let score = "33" // "33abc" / null / undefined
 
 console.log(typeof score);
 console.log(typeof(score));
@@ -17,8 +17,46 @@ console.log(booleanisLoggedIn);
 null => 0
 undefined => NaN
 */
-
+/*
 let somenum = 33
 let stringNum = String(somenum)
 console.log(typeof stringNum);
 console.log(stringNum);
+*/
+// ******Operations*******
+
+// let val = 3
+// let negval = -val
+// console.log(negval);
+// console.table([2+2, 2*2, 2/4, 2%5, 2**3])
+
+// str1 = "Hello"
+// str2 = "Ruchi"
+// str3 = str1 + str2
+// console.log(str3);
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+// console.log(num3);
+
+// let x = 3;
+// const y = x++;
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a=3
+// const b = ++a
+// console.log(`a:${a}, b:${b}`);
+
+// let z= x>y
+// console.log(z)
+
+//Comparison and equality check are diff. Their syntax is diff. avoid such comparisons.
+console.log(null>=0);
+console.log(null==0);
+console.log(null>0);
+
+// === (strict check)
+console.log("2" == 2); //true
+console.log("2" === 2); //false
